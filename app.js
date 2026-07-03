530073da3cd2d908ebdc8c1f1ced33e91448c08a
azamjonbro <azamjondasturchi11@gmail.com>
2026-07-03 18:52:57 +0500

    feat: restrict CORS policy to specific origins and define allowed methods and headers

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ const { swaggerUi, specs } = require('./swagger/index');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://alharameenadmin.netlify.app',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:3055'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
