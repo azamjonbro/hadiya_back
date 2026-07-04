5ed41a4bf7f997ead80a490e09de1ed1cb617147
azamjonbro <azamjondasturchi11@gmail.com>
2026-07-04 16:45:45 +0500

    feat: add additional allowed origins for CORS in app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(cors({
     'https://alharameenadmin.netlify.app',
     'https://alharameenuz.netlify.app',
     'https://alharameenuz.uz',
+    'https://www.alharameenuz.uz',
+    "http://www.alharameenuz.uz",
     'http://alharameenuz.uz',
     'http://localhost:5173',
     'http://localhost:3000',
